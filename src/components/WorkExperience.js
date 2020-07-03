f15a69044d104efba6a271ead0653796c66c3422
Rithvik <rithvik7@illinois.edu>
2020-07-02 22:47:05 -0400

    Updated work section with first 2 experiences and basic information

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react"
-import Typist from "react-typist"
-import mozilla from "../assets/images/mozilla.png"
-import uiuc from "../assets/images/uiuc.jpg"
-
+import { Mozilla, CS125 } from "./Work-List"
 
 export default class WorkExperience extends Component {
     render() {
@@ -17,39 +14,23 @@ export default class WorkExperience extends Component {
             <section id="work" className="work">
                 <div className="container">
                     <div className="row">
+
                         <div className="col-lg-6">  
                             <div className="work-terminal">
                                 <div className="col-lg-16 pt-8 pt-lg-0 content">
-                                    <span className = "text-center">
-                                        <span>
-                                            <Typist>
-                                            <Typist.Delay ms = {1000} />
-                                                <h3><span className="terminal-styling">Mozilla</span></h3>
-                                            </Typist>
-                                        </span>
-                                    </span>
-
+                                    <Mozilla />
                                 </div>
-
                             </div>
                         </div>
 
                         <div className="col-lg-6">  
                             <div className="work-terminal">
                                 <div className="col-lg-16 pt-8 pt-lg-0 content">
-                                    <span className = "text-center">
-                                        <span>
-                                            <Typist>
-                                            <Typist.Delay ms = {1000} />
-                                                <h3><span className="terminal-styling">Mozilla</span></h3>
-                                            </Typist>
-                                        </span>
-                                    </span>
-
+                                    <CS125 />
                                 </div>
-                                
                             </div>
                         </div>
+
                     </div>
                 </div>
             </section>
