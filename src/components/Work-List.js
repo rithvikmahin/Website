c490c00706fb5d4f8ebf59c0288731b970e39285
Rithvik <rithvik7@illinois.edu>
2020-07-05 19:34:06 -0400

    Time to delay increased

--- src/components/Work-List.js
+++ src/components/Work-List.js
@@ -10,7 +10,7 @@ export class Mozilla extends Component {
             <div className="col-lg-6">
               <span className = "text-nowrap">
                 <ul>
-                  <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Mozilla </span></strong></Typist></li>
+                  <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Mozilla </span></strong></Typist></li>
                   <li><strong><span className="terminal-wrapping">$ Developer in the Spring Open Lab </span></strong> </li>
                   <li><strong><span className="terminal-wrapping"> $ April 2020 - June 2020 | Remote </span></strong> </li>
                 </ul>
@@ -29,7 +29,7 @@ export class CS125 extends Component {
             <div className="col-lg-6">
               <span className = "text-nowrap">
                 <ul>
-                  <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />CS 125 - Introduction to Computer Science </span></strong></Typist></li>
+                  <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />CS 125 - Introduction to Computer Science </span></strong></Typist></li>
                   <li><strong><span className="terminal-wrapping">$ Course Developer for the class CS 125 </span></strong> </li>
                   <li><strong><span className="terminal-wrapping">$ February 2020 - Ongoing | Champaign, USA </span></strong> </li>
                 </ul>
@@ -48,7 +48,7 @@ export class IIMB extends Component {
           <div className="col-lg-6">
             <span className = "text-nowrap">
               <ul>
-                <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Indian Institute of Management </span></strong></Typist></li>
+                <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Indian Institute of Management </span></strong></Typist></li>
                 <li><strong><span className="terminal-wrapping">$ Data Science Intern </span></strong> </li>
                 <li><strong><span className="terminal-wrapping">$ May 2018 - July 2018 | Bangalore, India </span></strong> </li>
               </ul>
@@ -67,7 +67,7 @@ export class Visa extends Component {
           <div className="col-lg-6">
             <span className = "text-nowrap">
               <ul>
-                <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Visa </span></strong></Typist></li>
+                <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Visa </span></strong></Typist></li>
                 <li><strong><span className="terminal-wrapping">$ Software Engineering Intern </span></strong> </li>
                 <li><strong><span className="terminal-wrapping">$ February 2018 - July 2018 | Bangalore, India </span></strong> </li>
               </ul>
@@ -86,7 +86,7 @@ export class Collagro extends Component {
           <div className="col-lg-6">
             <span className = "text-nowrap">
               <ul>
-                <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Collagro </span></strong></Typist></li>
+                <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Collagro </span></strong></Typist></li>
                 <li><strong><span className="terminal-wrapping">$ Data Science Intern </span></strong> </li>
                 <li><strong><span className="terminal-wrapping">$ November 2017 - January 2018 | Bangalore, India </span></strong> </li>
               </ul>
@@ -105,7 +105,7 @@ export class Cisco extends Component {
           <div className="col-lg-6">
             <span className = "text-nowrap">
               <ul>
-                <li><Typist><Typist.Delay ms = {1500} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Cisco </span></strong></Typist></li>
+                <li><Typist><Typist.Delay ms = {3000} /><strong><span className="terminal-wrapping-title">$ Loading... <Typist.Backspace count={11} delay={500} />Cisco </span></strong></Typist></li>
                 <li><strong><span className="terminal-wrapping">$ Software Engineering Intern </span></strong> </li>
                 <li><strong><span className="terminal-wrapping">$ July 2017 - January 2018 | Bangalore, India </span></strong> </li>
               </ul>
