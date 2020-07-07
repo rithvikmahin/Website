7a18a85a93a949384fd09e828d052fcdc5a63e9a
Rithvik <rithvik7@illinois.edu>
2020-07-07 14:06:05 -0400

    Removed anchor tags for links on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react"
 import picture from '../assets/images/profile-picture.jpeg' /**TODO: Change image */
 import $ from "jquery"
+import Scroll from "react-scroll"
+const ScrollLink = Scroll.ScrollLink;
 
 export default class Navigation extends Component {
-
 
   MobileNavbar() {
     $(document).on('click', '.mobile-nav-toggle', function(e) {
@@ -31,12 +32,12 @@ export default class Navigation extends Component {
             </div>
             <nav className="nav-menu">
               <ul>
-                <li className="active"><a href="index.html"><i className="bx bx-home" /> <span>Home</span></a></li>
-                <li><a href="#about"><i className="bx bx-user" /> <span>About</span></a></li>
-                <li><a href="#experiences"><i className="bx bx-book-content" /> Work Experience</a></li>
-                <li><a href="#projects"><i className="bx bx-book-content" /> Projects</a></li>
-                <li><a href="#skills"><i className="bx bx-book-content" /> Skills</a></li>
-                <li><a href="#contact"><i className="bx bx-envelope" /> Contact Me</a></li>
+                <li className="navbar-links"><i className="bx bx-home" /> <span>Home</span></li>
+                <li className="navbar-links"><i className="bx bx-user" /> <span>About</span></li>
+                <li className="navbar-links"><i className="bx bx-code-block" /> Work Experience</li>
+                <li className="navbar-links"><i className="bx bxs-card" /> Projects</li>
+                <li className="navbar-links"><i className="bx bx-book-content" /> Skills</li>
+                <li className="navbar-links"><i className="bx bx-envelope" /> Contact Me</li>
               </ul>
             </nav>
           </div>
