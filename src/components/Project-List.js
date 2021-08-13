728676f320fb0e48742bc674667e9c666524d1a3
Rithvik <rithvik7@illinois.edu>
2021-08-13 14:54:55 -0400

    Updated CSS

--- src/components/Project-List.js
+++ src/components/Project-List.js
@@ -4,12 +4,8 @@ export default class ProjectList extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row mb-4">
-                    <div className="col-lg-6">
-                        <iframe src="https://uiuc-course-visualizer.herokuapp.com/" width="1125" height="500" frameBorder="0"></iframe>
-                    </div>
-                </div>
+            <div className="container fluid">
+                <iframe src="https://uiuc-course-visualizer.herokuapp.com/" width="100%" height="500" frameBorder="0"></iframe>
             </div>
         );
     }
